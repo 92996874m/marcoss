a35d6312871b17df58c42ccab75543cc86a0077f
zaymuel <samuelmartin2002@gmail.com>
2022-03-11 13:39:59 -0300

    implement newton OK

--- localizacao_raiz/newton.py
+++ localizacao_raiz/newton.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 import decimal
 # log = ln
-from sympy import Symbol, Float, Integer, N, lambdify, exp, sqrt, log, cos, sin
+from sympy import Symbol, diff, Float, Integer, N, lambdify, Abs, exp, sqrt, log, cos, sin, srepr
 import pandas as pd
 # sys.tracebacklimit = 0
 
@@ -15,21 +15,21 @@ x = Symbol('x', real=True)
 
 """""""""INPUTS"""""""""
 # N_REQ = número de casas decimais de precisão
-N_REQ = int(3)
+N_REQ = int(10)
 # N_PREC = número de casas decimais de precisão + 2 casas de sobra
 N_PREC = N_REQ + 2
 # COLOCAR NÚMEROS DECIMAIS ENTRE ASPAS!!!! SENÃO NÃO TEM PRECISÃO
 # A = extremo esquerdo
-A = Float(-1, N_PREC)
+A = Float(1, N_PREC)
 # B = extremo direito
-B = Float(1, N_PREC)
+B = Float(2, N_PREC)
 # adapted from https://stackoverflow.com/a/9877279
 # se `f(x)` é uma função qualquer de x
 # fazer f(x) = 0
 # isolar x de um lado. Tudo que sobrar do outro é chamado de `phi(x)`
 # de modo a resultar em x = phi(x)
 # trabalharemos apenas com a phi(x).
-phi = 1 / (1 + x**2)
+phi = x ** 2 - 2
 # ex: x**2 * exp(x) + sqrt(x) - ( 1 / log(x) ) * ( 1 / cos(x) ) * sin(x)
 # ex: se f(x) = x^3 - 7x + 2,
 # fazemos f(x) = => x^3 - 7x + 2 = 0
@@ -38,8 +38,15 @@ phi = 1 / (1 + x**2)
 """"""""""""""""""
 
 f = lambdify(x, phi, 'sympy')
-yprime = lambdify(x, phi.diff(x), 'sympy')
-print(yprime)
+try:
+    f_ = phi.diff(x)
+    lf_ = lambdify(x, f_, 'sympy')
+    lmodf_ = lambdify(x, Abs(f_), 'sympy')
+    lmodf__ = lambdify(x, Abs(diff(f_, x)), 'sympy')
+except:
+    print("ERRO!!!")
+    raise ValueError(
+        f"Não foi possível calcular derivadas do módulo da derivada da função f. Favor usar outro método.")
 
 # Teorema do Valor Intermediário (TVI)
 # verifica se existe _pelo menos 1_ raiz no intervalo
@@ -58,32 +65,33 @@ b = B
 # estimativa inicial
 c = Float((a + b) / Float(2, N_PREC), N_PREC)
 
-# fator de contração
-# VERIFICAR MANUALMENTE
-C = Float(3, N_PREC) * Float(math.sqrt(3), N_PREC) / Float(8, N_PREC)
-print("C manual:", C)
-# raiz quadrada: `math.sqrt()`
-
-"""cálculo automático do fator de contração"""
 # range de derivadas entre A e B
-expoente = abs(Integer(N_REQ - 2))
-derivadas_C = [
-    N(yprime(i/10**(expoente)), 2)
+expoente = abs(Integer(N_REQ % 2 + 1))
+derivadas_m = [
+    N(lmodf_(i/10**(expoente)), 2)
     for i in range(
-        int(A) * 10**(expoente),
-        int(B) * 10**(expoente)
+        int(A * 10**(expoente)),
+        int(B * 10**(expoente))
     )
 ]
-derivadas_C.extend([N(yprime(A), N_PREC), N(yprime(B), N_PREC)])
-# pegar a maior delas
-C = max(derivadas_C)
-print("C calculado:", C)
 
-# Fator de contração tem que ser menor que 1
-if C >= 1:
-    print("ERRO!!!")
-    raise ValueError(
-        f"Fator de contração maior que 1. Favor verificar e inserir manualmente o C.")
+derivadas_M = [
+    N(lmodf__(i/10**(expoente)), 2)
+    for i in range(
+        int(A * 10**(expoente)),
+        int(B * 10**(expoente))
+    )
+]
+
+# print(max(derivadas_C), [N(yprime(A), N_PREC), N(yprime(B), N_PREC)])
+derivadas_m.extend([N(lmodf_(A), N_PREC), N(lmodf_(B), N_PREC)])
+derivadas_M.extend([N(lmodf__(A), N_PREC), N(lmodf__(B), N_PREC)])
+# pegar a maior delas
+m = min(derivadas_m)
+M = max(derivadas_M)
+# print(derivadas_m)
+# print(derivadas_M)
+# print("m, M:", m, M)
 
 while(True):
     print(f"Etapa {n} em progresso...", end="\t", flush=True)
@@ -91,10 +99,10 @@ while(True):
     n += 1
     n_list.append(n), a_list.append(a), b_list.append(b), c_list.append(c)
 
-    if n < 2:
-        e = Float((abs(a) + abs(b)), N_PREC) / Float(2, N_PREC)
+    if n == 1:
+        e = Float(M / (2*m) * (abs(A-B)) ** 2, N_PREC)
     else:
-        e = Float(1/(1-C) * (C**n) * abs(c_list[0] - c_list[1]), N_PREC)
+        e = Float(M / (2*m) * (e_list[n-2]) ** 2, N_PREC)
 
     f_c = N(f(c), N_PREC)
     f_list.append(f_c), e_list.append(e)
@@ -107,10 +115,9 @@ while(True):
     if e < 10**(-N_REQ):
         break
     else:
-        c = c - ( f_c / yprime(c) )
+        c = c - ( f_c / lf_(c) )
 
 
-e_list[0] = Float(1/(1-C) * (C**1) * abs(c_list[0] - c_list[1]), N_PREC)
 print("\n", c)
 
 d = {'n': n_list, 'a': a_list, 'b': b_list,

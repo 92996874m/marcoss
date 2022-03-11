840d65b0315dc14ec57b532ee97f7d6ef7703804
zaymuel <samuelmartin2002@gmail.com>
2022-03-11 13:02:22 -0300

    restructure bisseccao

--- localizacao_raiz/bisseccao.py
+++ localizacao_raiz/bisseccao.py
@@ -3,37 +3,37 @@ import math
 import numpy as np
 import decimal
 # log = ln
-from sympy import Symbol, lambdify, exp, sqrt, log, cos, sin
+from sympy import Symbol, Float, lambdify, N, exp, sqrt, log, cos, sin
 import pandas as pd
 # import time
 
 x = Symbol('x', real=True)
 
 """""""""INPUTS"""""""""
-# N = número de casas decimais de precisão
-N = int(20)
+# N_REQ = número de casas decimais de precisão
+N_REQ = int(11)
+# N_PREC = número de casas decimais de precisão + 2 casas de sobra
+N_PREC = N_REQ + 2
 # COLOCAR NÚMEROS DECIMAIS ENTRE ASPAS!!!! SENÃO NÃO TEM PRECISÃO
 # A = extremo esquerdo
-A = decimal.Decimal("0")
+A = Float(0, N_PREC)
 # B = extremo direito
-B = decimal.Decimal("1")
+B = Float(20, N_PREC)
 # adapted from https://stackoverflow.com/a/9877279
 # y = função qualquer de x
-y = x*exp(x) - 2
+y = x - math.pi
 """"""""""""""""""
 
-f = lambdify(x, y, 'numpy')
-# yprime = f.diff(x)
-
-# Set decimal precision to 2 more than the required significant figures
-decimal.getcontext().prec = N + 2
+f = lambdify(x, y, 'sympy')
 
 # Teorema do Valor Intermediário (TVI)
 # verifica se existe _pelo menos 1_ raiz no intervalo
-if f(A) * f(B) > 0:
+if N(f(A), N_PREC) * N(f(B), N_PREC) > 0:
     sys.tracebacklimit = 0
     print("ERRO!!!")
-    raise ValueError(f"Favor alterar extremos A e B. Não existe raiz no atual intervalo [{A}, {B}]")
+    raise ValueError(
+        f"Favor alterar extremos A e B. Não existe raiz no atual intervalo [{A}, {B}]"
+    )
 
 n_list, a_list, b_list, c_list, f_list, e_list = [], [], [], [], [], []
 
@@ -44,11 +44,11 @@ b = B
 while(True):
     print(f"Etapa {n} em progresso", flush=True)
     # time.sleep(1)
-    c = decimal.Decimal((a + b) / decimal.Decimal("2"))
+    c = Float((a + b) / 2, N_PREC)
     n += 1
     n_list.append(n), a_list.append(a), b_list.append(b), c_list.append(c),
     e = (B - A) / (2**n)
-    f_c = f(c)
+    f_c = N(f(c), N_PREC)
     f_list.append(f_c), e_list.append(e),
     if f_c > 0:
         b = c
@@ -56,7 +56,7 @@ while(True):
         a = c
     else:
         break
-    if e < 10**(-N):
+    if e < 10**(-N_REQ):
         break
 print(c)
 
@@ -64,4 +64,4 @@ d = {'n': n_list, 'a': a_list, 'b': b_list,
      'c': c_list, 'f_c': f_list, 'e': e_list}
 df = pd.DataFrame(data=d)
 print(df)
-print(f"`{c_list[-1]}` é a melhor estimativa para a raiz `c` da função `f(x) = {y}`, com `{N}` casas decimais de precisão.")
+print(f"`{c_list[-1]}` é a melhor estimativa para a raiz `c` da função `{y}`, com `{N_REQ}` casas decimais de precisão.")
